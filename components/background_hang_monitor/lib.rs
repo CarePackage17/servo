ee8d646f2f1dae1b2478ab6f772ae256dffb5ba5
CarePackage17 <5157010+CarePackage17@users.noreply.github.com>
2025-06-26 11:35:15 +0200

    Adapt LinuxSampler to use backtrace crate instead of unwind-sys
    Signed-off-by: CarePackage17 <5157010+CarePackage17@users.noreply.github.com>

--- components/background_hang_monitor/lib.rs
+++ components/background_hang_monitor/lib.rs
@@ -16,12 +16,13 @@ mod sampler;
         target_env = "musl"
     ))
 ))]
-mod sampler_linux;
+// mod sampler_linux;
+mod sampler_backtrace;
+#[cfg(target_os = "android")]
+mod sampler_backtrace;
 #[cfg(all(feature = "sampler", target_os = "macos"))]
 mod sampler_mac;
 #[cfg(all(feature = "sampler", target_os = "windows"))]
 mod sampler_windows;
-#[cfg(target_os = "android")]
-mod sampler_linux;
 
 pub use self::background_hang_monitor::*;
